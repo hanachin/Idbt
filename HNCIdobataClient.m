4b9aaec7d1e7560c5d62ad936d6992908bea1f0e
Seiei Higa <hanachin@gmail.com>
2014-07-26 22:38:13 +0900

    Add HNCIdobataClient, just created empty class

---
+++ HNCIdobataClient.m
@@ -0,0 +1,13 @@
+//
+//  HNCIdobataClient.m
+//  Idbt
+//
+//  Created by Seiei Higa on 2014/07/26.
+//  Copyright (c) 2014年 Seiei Higa. All rights reserved.
+//
+
+#import "HNCIdobataClient.h"
+
+@implementation HNCIdobataClient
+
+@end
