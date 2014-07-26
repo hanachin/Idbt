082ae609aa8d089ee19c7e4e831a80b7426acf75
Seiei Higa <hanachin@gmail.com>
2014-07-26 21:28:33 +0900

    Initial Commit

---
+++ Idbt/HNCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HNCAppDelegate.h
+//  Idbt
+//
+//  Created by Seiei Higa on 2014/07/26.
+//  Copyright (c) 2014年 Seiei Higa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Idbt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Idbt
+//
+//  Created by Seiei Higa on 2014/07/26.
+//  Copyright (c) 2014年 Seiei Higa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HNCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HNCAppDelegate class]));
+    }
+}
