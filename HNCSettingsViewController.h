6165877765a8a74bbe4c11e17d91593434fa0c81
Seiei Higa <hanachin@gmail.com>
2014-07-26 22:07:39 +0900

    Add settings view controller and set IBOutlet email and password

---
+++ HNCSettingsViewController.h
@@ -0,0 +1,16 @@
+//
+//  HNCSettingsViewController.h
+//  Idbt
+//
+//  Created by Seiei Higa on 2014/07/26.
+//  Copyright (c) 2014年 Seiei Higa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNCSettingsViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *email;
+@property (weak, nonatomic) IBOutlet UITextField *password;
+
+@end
