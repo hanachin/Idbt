4aae62e827a0785dab9026c1c0ced888f709b31b
Seiei Higa <hanachin@gmail.com>
2014-07-26 21:37:51 +0900

    Hi from TopViewController

---
+++ HNCTopViewController.h
@@ -0,0 +1,13 @@
+//
+//  HNCTopViewController.h
+//  Idbt
+//
+//  Created by Seiei Higa on 2014/07/26.
+//  Copyright (c) 2014年 Seiei Higa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNCTopViewController : UIViewController
+
+@end
---
+++ HNCTopViewController.m
@@ -0,0 +1,39 @@
+//
+//  HNCTopViewController.m
+//  Idbt
+//
+//  Created by Seiei Higa on 2014/07/26.
+//  Copyright (c) 2014年 Seiei Higa. All rights reserved.
+//
+
+#import "HNCTopViewController.h"
+
+@interface HNCTopViewController ()
+
+@end
+
+@implementation HNCTopViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    NSLog(@"Hi");
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
