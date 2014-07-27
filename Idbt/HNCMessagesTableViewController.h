1973df7a86fee8c5157064d94af3bf7b5b47bad6
Seiei Higa <hanachin@gmail.com>
2014-07-27 20:22:02 +0900

    Add HNCMessagesTableView and set as initial view

---
+++ Idbt/HNCMessagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HNCMessagesTableViewController.h
+//  Idbt
+//
+//  Created by Seiei Higa on 2014/07/27.
+//  Copyright (c) 2014年 Seiei Higa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNCMessagesTableViewController : UITableViewController
+
+@end
