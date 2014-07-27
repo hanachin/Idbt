5d94b87849e4b96ea8e424ca81adbe1bf9029d13
Seiei Higa <hanachin@gmail.com>
2014-07-27 20:23:01 +0900

    messages table has one section

--- Idbt/HNCMessagesTableViewController.m
+++ Idbt/HNCMessagesTableViewController.m
@@ -44,9 +44,7 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
