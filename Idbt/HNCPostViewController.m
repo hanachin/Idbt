d5c6b0df6540cef2d4b488825d60510695661b9e
Seiei Higa <hanachin@gmail.com>
2014-08-02 19:26:52 +0900

    Add post view

---
+++ Idbt/HNCPostViewController.m
@@ -0,0 +1,49 @@
+//
+//  HNCPostViewController.m
+//  Idbt
+//
+//  Created by Seiei Higa on 2014/08/02.
+//  Copyright (c) 2014年 Seiei Higa. All rights reserved.
+//
+
+#import "HNCPostViewController.h"
+
+@interface HNCPostViewController ()
+
+@end
+
+@implementation HNCPostViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
