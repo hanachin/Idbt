765cbbe66fc5206af337475d8158238ac50668fa
Seiei Higa <hanachin@gmail.com>
2014-08-15 14:35:59 +0900

    HNCIdbtを作った。ここにいろいろ詰め込みたい。

---
+++ Idbt/HNCIdbt.h
@@ -0,0 +1,13 @@
+//
+//  HNCIdbt.h
+//  Idbt
+//
+//  Created by Seiei Higa on 2014/08/15.
+//  Copyright (c) 2014年 Seiei Higa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HNCIdbt : NSObject
+
+@end
