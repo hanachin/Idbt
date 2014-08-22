195e23d8df0c1365abbf1cdb2cabaa4aab98714b
Seiei Higa <hanachin@gmail.com>
2014-08-22 15:14:57 +0900

    タイムラインの古いメッセージ読むメソッドをHNCIdobataClientに足した

--- Idbt/HNCIdobataClient.m
+++ Idbt/HNCIdobataClient.m
@@ -62,6 +62,13 @@
     [self messages:url completeHandler:completionHandler];
 }
 
+- (void)messagesBefore:(NSInteger)messageId completionHandler:(void (^)(NSArray *messages, NSURLResponse *response, NSError *error))completionHandler
+{
+    NSURL *url = [NSURL URLWithString: [NSString stringWithFormat: @"https://idobata.io/api/messages?older_than=%ld", (long)messageId]];
+    [self messages:url completeHandler:completionHandler];
+}
+
+
 - (void)roomMessages:(NSInteger)roomId before:(NSInteger)messageId completionHandler:(void (^)(NSArray *messages, NSURLResponse *response, NSError *error))completionHandler
 {
     NSURL *url = [NSURL URLWithString: [NSString stringWithFormat: @"https://idobata.io/api/messages?room_id=%ld&older_than=%ld", (long)roomId, (long)messageId]];
